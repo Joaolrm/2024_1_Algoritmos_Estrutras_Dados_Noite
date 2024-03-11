1f32ec6586ca0b724166dd9dd1adfea1f5b472c4
JOÃO LUIS ROSA DE MOURA <632310027@fspoa.br>
2024-03-11 20:00:04 -0300

    a

---
+++ Aula01/exercicio.py
@@ -0,0 +1,9 @@
+class Produtos():
+    def __init__(self):
+        self.nomes_produto = ["Rosas", "Violetas", "Margaridas"]
+        self.precos_produtos = ["23,50", "17,50", "13,50"]
+        self.qtd_produto = ["3", "7", "12"]
+    
+    
+
+    a
