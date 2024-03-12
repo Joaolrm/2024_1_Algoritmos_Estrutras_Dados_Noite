b876b5b481c9fc4e14f5687c4bcc97b10261e741
João <jlluismr86@gmail.com>
2024-03-11 21:38:19 -0300

    v3

---
+++ Aula02/main.py
@@ -0,0 +1,7 @@
+from Pessoa import Pessoa
+
+p1 = Pessoa("Jorge", 23)
+print(f"Nome da pessoa {p1.nome}")
+p2 = Pessoa(idade = 25, nome = "João")
+p3 = Pessoa("Carlos")
+print(f"Idade do {p3.nome} é {p3.idade}")
